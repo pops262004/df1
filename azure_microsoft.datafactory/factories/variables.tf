ac48f6f009b4c8eb054615ad1f1c5ce28e341951
pops262004 <trail262004@gmail.com>
2026-03-09 14:08:00 +0530

    update

---
+++ azure_microsoft.datafactory/factories/variables.tf
@@ -0,0 +1,8 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Target resource group name"
+}
+variable "virtual_network_name" {
+  type        = string
+  description = "Existing virtual network name"
+}
